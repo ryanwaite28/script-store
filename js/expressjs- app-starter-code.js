0ec3232ad9aaa89348dca6803c784456514f6241
Ryan M. Waite <ryanwaite28@gmail.com>
2018-03-06 10:32:06 -0500

    Add files via upload

---
+++ js/expressjs- app-starter-code.js
@@ -0,0 +1,28 @@
+var pages = {
+  vault: __dirname + '/vault/',
+
+  html_path: __dirname + '/vault/html/',
+  css_path: __dirname + '/vault/css/',
+  js_path: __dirname + '/vault/js/',
+  cdn_path: __dirname + '/vault/_cdn/',
+
+  imgPath: {
+    rel: '/images/',
+    abs: __dirname + '/vault/images/',
+  }
+
+  welcome: 'welcome.html',
+  signup: 'signup.html',
+  signin: 'signin.html',
+  signout: 'signout.html',
+
+  about: 'about.html',
+  faq: 'faq.html',
+  contact: 'contact.html',
+  info: 'info-1.html',
+
+  home: 'home.html',
+
+  generic: 'generic.html',
+  error: 'error.html',
+}
