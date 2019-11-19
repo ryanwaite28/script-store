6b313566be0b3fb1fddbbb4ee32be011634e209f
Ryan M. Waite <ryanwaite28@gmail.com>
2019-11-19 16:40:06 -0500

    Add files via upload

--- js/math-exponential-change.js
+++ js/math-exponential-change.js
@@ -5,9 +5,14 @@
  * @param t = time in years
  * @param r = rate
  * @param di = decrease or increase. `true` = increase; `false` = decrease
+ * @param f = format to 2 decimal places, default is `true`
  */
-function expChg(p, n, t, r, di = true) {
-  return di 
+function expChg(p, n, t, r, di = true, f = true) {
+  const answer = di 
     ? p * ((1 + (r / n)) ** (n * t))
     : p * ((1 - (r / n)) ** (n * t));
+
+  return f
+    ? Math.round(answer * 100) / 100
+    : answer;
 }
