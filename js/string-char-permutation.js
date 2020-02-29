28b1ac0ef9dfda1fb7a76fa411eafb3df320a014
Ryan M. Waite <ryanwaite28@gmail.com>
2020-02-29 12:39:35 -0500

    Add files via upload

--- js/string-char-permutation.js
+++ js/string-char-permutation.js
@@ -1,10 +1,27 @@
+/**
+ * String permutation
+ * ---
+ *
+ * Given a string, a target substring and a list of strings to replace with,
+ * this function returns an array of all permutations.
+ * 
+ * Example:
+ * - permutateString('0?1?', '?', ['0', '1']) → ['0010', '0011', '0110', '0111']
+ *
+ * @param {string} str 
+ * @param {string} char 
+ * @param {array} permChars 
+ * @returns {array}
+ */
+
 function permutateString(
   str,
   char,
   permChars
 ) {
   // if there are no question marks, return
-  if (!str.includes('?')) {
+  const doesNotHaveChar = !str.includes(char);
+  if (doesNotHaveChar) {
     return [str];
   } else if (str === char) {
     return [...permChars];
@@ -25,6 +42,3 @@ function permutateString(
 
   return listOfPermutations;
 }
-
-const c = permutateString('0?0?', '?', ['a', 'b']);
-console.log(c);
